0db7bf82d92c0caca17969c0e570f23f77b051c6
Rohan Nagalkar <rohan@x1.co>
2024-04-22 23:10:23 -0400

    external links done

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ Personal Website
 Feature List
 - [X] Landing Page with Name
 - [X] Cool Background
-- [X] Github workflow
-- [ ] Links to externals
+- [X] Github workflow (since I'll forget this later - to deploy, `npm run deploy` will push to gh-pages, which will trigger pipeline)
+- [X] Links to externals
 - [ ] About Me page
 - [ ] Booklist page
+- [ ] Mobile optimize
 - [ ] Cool Background that's animated ([example](https://codepen.io/matteobruni/pen/bGrXJew), I've spent too much time trying to figure this out)
--- src/HomePage.js
+++ src/HomePage.js
@@ -8,10 +8,10 @@ const HomePage = () => {
         Rohan Nagalkar
       </div>
       <div className="social-icons center-inline-text" style={{ marginTop: '50px' }}>
-        <a href="https://github.com/rohan-n"><i className="fab fa-github-square fa-3x fa-fw" style={{ color: 'white', marginRight: '40px' }}></i></a>
-        <a href="https://www.linkedin.com/in/rohannagalkar/"><i className="fab fa-linkedin fa-3x fa-fw" style={{ color: 'white', marginRight: '40px' }}></i></a>
-        <a href="https://twitter.com/rohranger"><i className="fab fa-x-twitter fa-3x fa-fw" style={{ color: 'white', marginRight: '40px' }}></i></a>
-        <a href="mailto:rohan.nagalkar13@gmail.com"><i className="fa fa-envelope fa-3x fa-fw" style={{ color: 'white', marginRight: '40px' }}></i></a>
+        <a href="https://github.com/rohan-n"><i className="fab fa-github-square fa-3x fa-fw"></i></a>
+        <a href="https://www.linkedin.com/in/rohannagalkar/"><i className="fab fa-linkedin fa-3x fa-fw"></i></a>
+        <a href="https://twitter.com/rohranger"><i className="fab fa-x-twitter fa-3x fa-fw" ></i></a>
+        <a href="mailto:rohan.nagalkar13@gmail.com"><i className="fa fa-envelope fa-3x fa-fw"></i></a>
       </div>
     </div>
   );
